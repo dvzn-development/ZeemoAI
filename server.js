b9ef6c350f56aa526419ac430c801cc6a0d81656
dvzn (formerly soloo) <central@soloo.win>
2025-08-07 19:21:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
+
 app.use('/api/chat', require('./api/chat'));
+
 app.use(express.static('public'));
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
